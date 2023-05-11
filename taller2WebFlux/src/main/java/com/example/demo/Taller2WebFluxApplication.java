2d61e8db5a8ab0c8c8b9030666f58f943ed201b0
dmateome-nttdata <130351288+dmateome-nttdata@users.noreply.github.com>
2023-05-11 11:28:35 +0200

    taller2 Web FLux

---
+++ taller2WebFlux/src/main/java/com/example/demo/Taller2WebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2WebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller2WebFluxApplication.class, args);
+	}
+
+}
